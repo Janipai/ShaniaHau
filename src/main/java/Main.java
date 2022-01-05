aff4a44bc3bf0913eef76bd0703922d9430bc169
Janipai <s195477@student.dtu.dk>
2022-01-05 07:08:15 +0100

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello 02267 Software development of web services");
+        System.out.println("s195477 Shania Hau");
+    }
+}
